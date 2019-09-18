1d21b7af600d7abd63e7ec50a38749dc8324c181
Tommy Larsson <45052383+larssont@users.noreply.github.com>
2019-09-18 03:38:57 +0200

    Set version number to 0.0.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="pyombi",
-    version="0.0.3",
+    version="0.0.4",
     url="https://github.com/larssont/pyombi",
     author="Tommy Larsson",
     author_email="larssont@tuta.io",
