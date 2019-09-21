3ae328b664d0d41a47ce41b9ead246201a63c1ee
Tommy Larsson <45052383+larssont@users.noreply.github.com>
2019-09-21 20:00:04 +0200

    Increment version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="pyombi",
-    version="0.1.0",
+    version="0.1.1",
     url="https://github.com/larssont/pyombi",
     author="Tommy Larsson",
     author_email="larssont@tuta.io",
