c9e39936bceae23a02d19009d99210f880d884f5
Tommy Larsson <45052383+larssont@users.noreply.github.com>
2019-09-22 16:41:53 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="pyombi",
-    version="0.1.3",
+    version="0.1.4",
     url="https://github.com/larssont/pyombi",
     author="Tommy Larsson",
     author_email="larssont@tuta.io",
