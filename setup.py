7be7aa5a0c9640938267ae5f4d31ac1a4bec5575
Tommy Larsson <45052383+larssont@users.noreply.github.com>
2020-06-24 07:24:52 +0200

    Revert "Update to version 0.2.0"

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="pyombi",
-    version="0.2.0",
+    version="0.1.10",
     url="https://github.com/larssont/pyombi",
     author="Tommy Larsson",
     author_email="larssont@tuta.io",
